72b6bf0915f64083032386926cbbb927cc1c5ad2
Tossaworn Srinutchasart <tossawornsri@gmail.com>
2026-06-25 14:34:02 +0700

    style(auth): update login page title

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -66,7 +66,7 @@ export default function LoginForm() {
   <div className="min-h-screen flex items-center justify-center">
     <Card className="w-full sm:max-w-md">
       <CardHeader>
-        <CardTitle>Login</CardTitle>
+        <CardTitle>Login 111 222</CardTitle>
         <CardDescription>
           Enter your email and password to login
         </CardDescription>
